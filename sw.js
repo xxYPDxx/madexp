985161c0ee2694927af77f6fc7d0fce73b8ab8a9
xxYPDxx <yuktant.dhande@gmail.com>
2022-04-11 09:59:55 +0530

    first

---
+++ sw.js
@@ -0,0 +1,67 @@
+if('serviceWorker' in navigator){
+    window.addEventListener('load', function(){
+        navigator.serviceWorker.register('sw.js').then(function (registration){
+            console.log('serviceWorker registration successful with scope', registration.scope);
+        }, function (err) {
+            console.log("ServiceWorker registration failed",err);
+        });
+    })
+}
+
+  
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+'/',
+'index.html',
+/* '/styles/main.css',
+'/script/main.js' */
+];
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+  self.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+  
+    self.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
+  });
